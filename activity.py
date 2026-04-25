36c44233c717ad2c527b2f7f3f2817e74a806907
afroze456 <afreenafroze1@gmail.com>
2026-04-25 14:23:39 -0700

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,29 @@
+x=5
+
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+
+
+x=5.5
+
+if (type(x) is not float):
+    print(true)
+else:
+    print("false")
+
+x=30
+y=40
+
+if (x is y):
+    print("x is y")
+else:
+    print("x isnt y")
+
+
+if (x is not y):
+    print("x isnt y")
+else:
+    print("x is y")
+
